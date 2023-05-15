832b9b59d6615d517d91b09fa9241f1828140de5
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-15 11:19:28 +0100

    Changes made to initial code

--- calculator.py
+++ calculator.py
@@ -1,10 +1,7 @@
 # Welcome messages 
-print("Welcome to my Python calculator!")
-print("You can Add, Subtract, Multiply and Divide")
-print("Type what would you like to do? \nAdd (+) \nSubtract (-) \nMultiply (*) \nDivide (/)")
-
-# Get user input
-user_input = input("Type the name or symbol of the mathematical function you would like to use: ")
+print("Welcome to my Python calculator!\n")
+print("You can Add, Subtract, Multiply and Divide\n")
+print("Type what would you like to do? \n1.Add (+) \n2.Subtract (-) \n3.Multiply (*) \n4.Divide (/)\n")
 
 # Math functions
 def add(a, b):
@@ -23,6 +20,19 @@ def divide(a, b):
 # Swith to that math function
 while True:
     
+    # Get user input
+    user_input = input("Type the number (1, 2, 3, 4) of the mathematical operation you would like to use: ")
+    
+    if user_input in ("1", "2", "3", "4"):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+        except ValueError:
+            print("Invalid input, please enter either 1, 2, 3, 4")
+            
+     
+     
+                       
     if user_input == "Add" or user_input == "+":
         print("You've chosen addition!")
             
